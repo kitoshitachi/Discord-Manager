70378db480b22687a5ffd433f7a4311e5fbf2906
kitoshitachi <19521204@gm.uit.edu.vn>
2023-09-23 03:44:36 +0000

    fix with Vnick

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ Version: 6.1.0
 import os
 import platform
 import random
-
+from keep_alive import keep_alive
 from logger import Logger
 from datetime import datetime
 from settings import DISCORD_TOKEN, LOGS_CHANNEL, SPECIAL_ROLE
@@ -22,162 +22,162 @@ from discord.ext.commands import Context
 
 from utils import embed_message
 
-intents=discord.Intents.default()
+intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 intents.presences = True
 
+
 class DiscordBot(commands.Bot):
-    def __init__(self) -> None:
-        super().__init__(
-            command_prefix=commands.when_mentioned_or('v','V'),
-            intents=intents,
-            help_command=None,
-        )
-        self.logger = Logger
-        self.logs_channel = self.get_partial_messageable(int(LOGS_CHANNEL))
 
+  def __init__(self) -> None:
+    super().__init__(
+        command_prefix=commands.when_mentioned_or('v', 'V'),
+        intents=intents,
+        help_command=None,
+    )
+    self.logger = Logger
+    self.logs_channel = self.get_partial_messageable(int(LOGS_CHANNEL))
 
-    async def load_cogs(self) -> None:
-        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
-            if file.endswith(".py"):
-                extension = file[:-3]
-                try:
-                    await self.load_extension(f"cogs.{extension}")
-                    self.logger.info(f"Loaded extension '{extension}'")
+  async def load_cogs(self) -> None:
+    for file in os.listdir(
+        f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+      if file.endswith(".py"):
+        extension = file[:-3]
+        try:
+          await self.load_extension(f"cogs.{extension}")
+          self.logger.info(f"Loaded extension '{extension}'")
 
-                except Exception as e:
-                    exception = f"{type(e).__name__}: {e}"
-                    self.logger.error(
-                        f"Failed to load extension {extension}\n{exception}"
-                    )
-                    embed = discord.Embed(
-                        title=f"Error load cogs",
-                        description=f"Failed to load extension {extension}\n{exception}", 
-                        timestamp=datetime.now(),
-                        color=discord.Color.red()
-                    )
+        except Exception as e:
+          exception = f"{type(e).__name__}: {e}"
+          self.logger.error(
+              f"Failed to load extension {extension}\n{exception}")
+          embed = discord.Embed(
+              title=f"Error load cogs",
+              description=f"Failed to load extension {extension}\n{exception}",
+              timestamp=datetime.now(),
+              color=discord.Color.red())
 
-                    await self.logs_channel.send(embed=embed)
+          await self.logs_channel.send(embed=embed)
 
-    @tasks.loop(minutes=5.0)
-    async def status_task(self) -> None:
-        """
+  @tasks.loop(minutes=5.0)
+  async def status_task(self) -> None:
+    """
         Setup the game status task of the bot.
         """
-        Activities=[
-            discord.CustomActivity(name="Drinking !" , emoji='🍷'),
-            discord.CustomActivity(name="Fighting !" , emoji='⚔️'),
-            discord.CustomActivity(name="Sleeping !" , emoji='⚰️'),
-        ]
-        await self.change_presence(status= discord.Status.online, activity=random.choice(Activities))
+    Activities = [
+        discord.CustomActivity(name="Drinking !", emoji='🍷'),
+        discord.CustomActivity(name="Fighting !", emoji='⚔️'),
+        discord.CustomActivity(name="Sleeping !", emoji='⚰️'),
+    ]
+    await self.change_presence(status=discord.Status.online,
+                               activity=random.choice(Activities))
 
-    @status_task.before_loop
-    async def before_status_task(self) -> None:
-        """
+  @status_task.before_loop
+  async def before_status_task(self) -> None:
+    """
         Before starting the status changing task, we make sure the bot is ready
         """
-        await self.wait_until_ready()
+    await self.wait_until_ready()
 
-    async def setup_hook(self) -> None:
-        """
+  async def setup_hook(self) -> None:
+    """
         This will just be executed when the bot starts the first time.
         """
-        self.logger.info(f"Logged in as {self.user.name}")
-        self.logger.info(f"discord.py API version: {discord.__version__}")
-        self.logger.info(f"Python version: {platform.python_version()}")
-        self.logger.info(
-            f"Running on: {platform.system()} {platform.release()} ({os.name})"
-        )
-        self.logger.info("-------------------")
-        
-        await self.load_cogs()
-        self.status_task.start()
+    self.logger.info(f"Logged in as {self.user.name}")
+    self.logger.info(f"discord.py API version: {discord.__version__}")
+    self.logger.info(f"Python version: {platform.python_version()}")
+    self.logger.info(
+        f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    self.logger.info("-------------------")
 
-    async def on_message(self, message: discord.Message) -> None:
-        """
+    await self.load_cogs()
+    self.status_task.start()
+
+  async def on_message(self, message: discord.Message) -> None:
+    """
         The code in this event is executed every time someone sends a message, with or without the prefix
 
         :param message: The message that was sent.
         """
-        if message.author == self.user or message.author.bot:
-            return
-        await self.process_commands(message)
+    if message.author == self.user or message.author.bot:
+      return
+    await self.process_commands(message)
 
-    async def on_command_completion(self, context: Context) -> None:
-        """
+  async def on_command_completion(self, context: Context) -> None:
+    """
         The code in this event is executed every time a normal command has been *successfully* executed.
 
         :param context: The context of the command that has been executed.
         """
-        full_command_name = context.command.qualified_name
-        split = full_command_name.split(" ")
-        executed_command = str(split[0])
-         
-        content = f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
-        self.logger.info(
-            content
-        )
-        await self.logs_channel.send(embed=embed_message(
-            title=f"Executed {executed_command} command",
-            ctx=context,
-            color=discord.Color.green()
-        ))
+    full_command_name = context.command.qualified_name
+    split = full_command_name.split(" ")
+    executed_command = str(split[0])
 
-    async def on_command_error(self, context: Context, error) -> None:
-        """
+    content = f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
+    self.logger.info(content)
+    await self.logs_channel.send(
+        embed=embed_message(title=f"Executed {executed_command} command",
+                            ctx=context,
+                            color=discord.Color.green()))
+
+  async def on_command_error(self, context: Context, error) -> None:
+    """
         The code in this event is executed every time a normal valid command catches an error.
 
         :param context: The context of the normal command that failed executing.
         :param error: The error that has been faced.
         """
 
-        if isinstance(error, commands.CommandOnCooldown):
-            minutes, seconds = divmod(error.retry_after, 60)
-            hours, minutes = divmod(minutes, 60)
-            hours = hours % 24
-            embed = discord.Embed(
-                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
-                color=0xE02B2B,
-            )
-            await context.channel.send(embed=embed, delete_after=10)            
-        elif isinstance(error, commands.MissingPermissions):
-            embed = discord.Embed(
-                description="You are missing the permission(s) `"
-                + ", ".join(error.missing_permissions)
-                + "` to execute this command!",
-                color=0xE02B2B,
-            )
-            await context.channel.send(embed=embed)
-        elif isinstance(error, commands.BotMissingPermissions):
+    if isinstance(error, commands.CommandOnCooldown):
+      minutes, seconds = divmod(error.retry_after, 60)
+      hours, minutes = divmod(minutes, 60)
+      hours = hours % 24
+      embed = discord.Embed(
+          description=
+          f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+          color=0xE02B2B,
+      )
+      await context.channel.send(embed=embed, delete_after=10)
+    elif isinstance(error, commands.MissingPermissions):
+      embed = discord.Embed(
+          description="You are missing the permission(s) `" +
+          ", ".join(error.missing_permissions) + "` to execute this command!",
+          color=0xE02B2B,
+      )
+      await context.channel.send(embed=embed)
+    elif isinstance(error, commands.BotMissingPermissions):
 
-            embed = discord.Embed(
-                description="I am missing the permission(s) `"
-                + ", ".join(error.missing_permissions)
-                + "` to fully perform this command!",
-                color=0xE02B2B,
-            )
-            await context.channel.send(embed=embed)
+      embed = discord.Embed(
+          description="I am missing the permission(s) `" +
+          ", ".join(error.missing_permissions) +
+          "` to fully perform this command!",
+          color=0xE02B2B,
+      )
+      await context.channel.send(embed=embed)
 
-    async def on_member_update(self, before: Member, after: Member) -> None:   
-        special_role = get(after.guild.roles, id=int(SPECIAL_ROLE))
+  async def on_member_update(self, before: Member, after: Member) -> None:
+    special_role = get(after.guild.roles, id=int(SPECIAL_ROLE))
 
-        if before.top_role.name != after.top_role.name:
-            display_name = after.display_name #or after.name
-            if after.top_role.position > special_role.position: 
-                if before.top_role.position > special_role.position:
-                    display_name = display_name.split(' ')
-                    display_name.pop()
-                    display_name = ' '.join(display_name)
+    if before.top_role.name != after.top_role.name:
+      display_name = after.display_name  #or after.name
+      if after.top_role.position > special_role.position:
+        if before.top_role.position > special_role.position:
+          display_name = display_name.split(' ')
+          display_name.pop()
+          display_name = ' '.join(display_name)
 
-                display_name = f"{display_name} {after.top_role.name.split(' ')[-1]}"
+        display_name = f"{display_name} {after.top_role.name.split(' ')[-1]}"
 
-            elif after.top_role.position < special_role.position and before.top_role.position > special_role.position:
-                display_name = display_name.split(' ')
-                display_name.pop()
-                display_name = ' '.join(display_name)
+      elif after.top_role.position < special_role.position and before.top_role.position > special_role.position:
+        display_name = display_name.split(' ')
+        display_name.pop()
+        display_name = ' '.join(display_name)
 
-            await after.edit(nick = display_name)
+      await after.edit(nick=display_name)
+
+
+keep_alive()
 
 bot = DiscordBot()
 bot.run(DISCORD_TOKEN)
--- cogs/moderator.py
+++ cogs/moderator.py
@@ -16,80 +16,91 @@ from settings import SPECIAL_ROLE, LOGS_CHANNEL
 
 
 class Moderator(commands.Cog, name="Moderator"):
-    def __init__(self, bot):
-        self.bot = bot
 
+  def __init__(self, bot):
+    self.bot = bot
 
-    # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
-    @commands.hybrid_command(
-        name="clear",
-        description="Delete a number of messages.",
-    )
-    @commands.has_permissions(manage_messages=True)
-    @commands.bot_has_permissions(manage_messages=True)
-    @app_commands.describe(amount="The amount of messages that should be deleted.")
-    async def clear(self, ctx: Context, amount: int) -> None:
-        """
+  # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
+  @commands.hybrid_command(
+      name="clear",
+      description="Delete a number of messages.",
+  )
+  @commands.has_permissions(manage_messages=True)
+  @commands.bot_has_permissions(manage_messages=True)
+  @app_commands.describe(
+      amount="The amount of messages that should be deleted.")
+  async def clear(self, ctx: Context, amount: int) -> None:
+    """
         Delete a number of messages.
 
         :param context: The hybrid command ctx.
         :param amount: The number of messages that should be deleted.
         """
-        await ctx.send("Deleting messages...")
-        if amount > 99:
-            amount = 99
+    await ctx.send("Deleting messages...")
+    if amount > 99:
+      amount = 99
 
-        purged_messages = await ctx.channel.purge(limit=amount + 1)
-        embed = discord.Embed(
-            description=f"**{ctx.author}** cleared **{len(purged_messages)-1}** messages!",
-            color=0xBEBEFE,
-        )
-        await ctx.channel.send(embed=embed, delete_after=10)
+    purged_messages = await ctx.channel.purge(limit=amount + 1)
+    embed = discord.Embed(
+        description=
+        f"**{ctx.author}** cleared **{len(purged_messages)-1}** messages!",
+        color=0xBEBEFE,
+    )
+    await ctx.channel.send(embed=embed, delete_after=10)
 
-    @commands.hybrid_command(
-        name="nick",
-        description="Change the nickname of a user on a server.",
-    )
-    @commands.bot_has_permissions(manage_nicknames=True)
-    @app_commands.describe(
-        user="The user that should have a new nickname.",
-        nickname="The new nickname that should be set.",
-    )
-    async def nick(self, ctx: Context, user: discord.User, *, nickname: str = None) -> None:
-        """
+  @commands.hybrid_command(
+      name="nick",
+      description="Change the nickname of a user on a server.",
+  )
+  @commands.bot_has_permissions(manage_nicknames=True)
+  @app_commands.describe(
+      user="The user that should have a new nickname.",
+      nickname="The new nickname that should be set.",
+  )
+  async def nick(self,
+                 ctx: Context,
+                 user: discord.User,
+                 *,
+                 nickname: str = None) -> None:
+    """
         Change the nickname of a user on a server.
 
         :param context: The hybrid command ctx.
         :param user: The user that should have its nickname changed.
         :param nickname: The new nickname of the user. Default is None, which will reset the nickname.
         """
-        
-        member = ctx.guild.get_member(user.id) or await ctx.guild.fetch_member(user.id)
-        special_role = get(ctx.message.guild.roles, id=int(SPECIAL_ROLE))
 
-        if member.top_role.position > special_role.position:
-            nickname = f"{nickname or member.display_name} {member.top_role.name.split(' ')[-1]}"
+    member = ctx.guild.get_member(user.id) or await ctx.guild.fetch_member(
+        user.id)
+    special_role = get(ctx.message.guild.roles, id=int(SPECIAL_ROLE))
 
-        await member.edit(nick=nickname)
+    if member.top_role.position > special_role.position:
+      nickname = f"{nickname or member.display_name} {member.top_role.name.split(' ')[-1]}"
 
-    @nick.error
-    async def missing_user(self, ctx:Context, error):
-        if isinstance(error, commands.UserNotFound) or isinstance(error, commands.MissingRequiredArgument):
-            member = ctx.author
-            special_role = get(ctx.message.guild.roles, id=int(SPECIAL_ROLE))
+    await member.edit(nick=nickname)
 
-            nickname = ctx.message.content.strip().removeprefix('vnick')
-            await ctx.author.edit(nick=nickname)
-            if member.top_role.position > special_role.position:
-                await ctx.author.edit(nick=ctx.author.display_name + ' ' + member.top_role.name.split(' ')[-1])
-            
-        else:
-            await ctx.guild.get_channel(LOGS_CHANNEL).send(embed=discord.Embed(
-                title=f"Error nick command!",
-                description=str(error).capitalize(),
-                color=0xE02B2B,
-            ))  
+  @nick.error
+  async def missing_user(self, ctx: Context, error):
+    if isinstance(error, commands.UserNotFound) or isinstance(
+        error, commands.MissingRequiredArgument):
+      member = ctx.author
+      special_role = get(ctx.message.guild.roles, id=int(SPECIAL_ROLE))
+
+      nickname = ctx.message.content.strip().removeprefix(
+          'vnick').removeprefix('Vnick')
+      await ctx.author.edit(nick=nickname)
+      if member.top_role.position > special_role.position:
+        await ctx.author.edit(nick=ctx.author.display_name + ' ' +
+                              member.top_role.name.split(' ')[-1])
+
+    else:
+      await ctx.message.send(embed=discord.Embed(
+          title=f"Error nick command!",
+          description=str(error).capitalize(),
+          color=0xE02B2B,
+      ))
+
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot):
-    await bot.add_cog(Moderator(bot))
+  await bot.add_cog(Moderator(bot))
