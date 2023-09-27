425d0f6bd25e6d0c69df48515e9ed1a9105453dc
kitoshitachi <19521204@gm.uit.edu.vn>
2023-09-28 00:09:11 +0700

    add template and fix import same template

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -9,76 +9,76 @@ Version: 6.1.0
 import re
 import discord
 from discord import app_commands
-from discord.ext import commands
-from discord.ext.commands import Context
+from discord.ext.commands import (
+  Context, Cog,
+  hybrid_command, has_permissions, bot_has_permissions
+)
 from discord.utils import get
 from settings import SPECIAL_ROLE
 # Here we name the cog and create a new class for the cog.
 
 
-class Moderator(commands.Cog, name="Moderator"):
+class Moderator(Cog, name="Moderator"):
 
   def __init__(self, bot):
     self.bot = bot
 
   # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
-  @commands.hybrid_command(
-      name="clear",
-      description="Delete a number of messages.",
+  @hybrid_command(
+    name="clear",
+    description="Delete a number of messages.",
   )
-  @commands.has_permissions(manage_messages=True)
-  @commands.bot_has_permissions(manage_messages=True)
+  @has_permissions(manage_messages=True)
+  @bot_has_permissions(manage_messages=True)
   @app_commands.describe(
-      amount="The amount of messages that should be deleted.")
+    amount="The amount of messages that should be deleted.")
   async def clear(self, ctx: Context, amount: int) -> None:
     """
-        Delete a number of messages.
+      Delete a number of messages.
 
-        :param context: The hybrid command ctx.
-        :param amount: The number of messages that should be deleted.
-        """
+      :param context: The hybrid command ctx.
+      :param amount: The number of messages that should be deleted.
+      """
     await ctx.send("Deleting messages...")
     if amount > 99:
       amount = 99
 
     purged_messages = await ctx.channel.purge(limit=amount + 1)
     embed = discord.Embed(
-        description=
-        f"**{ctx.author}** cleared **{len(purged_messages)-1}** messages!",
-        color=0xBEBEFE,
+      description=f"**{ctx.author}** cleared **{len(purged_messages)-1}** messages!",
+      color=0xBEBEFE,
     )
     await ctx.channel.send(embed=embed, delete_after=10)
 
-  @commands.hybrid_command(
-      name="nick",
-      description="Change the nickname of a user on a server.",
+  @hybrid_command(
+    name="nick",
+    description="Change the nickname of a user on a server.",
   )
-  @commands.bot_has_permissions(manage_nicknames=True)
+  @bot_has_permissions(manage_nicknames=True)
   @app_commands.describe(
-      nickname="The new nickname that should be set.",
+    nickname="The new nickname that should be set.",
   )
-  async def nick(self,
-                 ctx: Context,
-                 *,
-                 nickname: str = None) -> None:
+  async def nick(self, ctx: Context, *, nickname: str = None) -> None:
     """
-        Change the nickname of a user on a server.
+      Change the nickname of a user on a server.
 
-        :param context: The hybrid command ctx.
-        :param user: The user that should have its nickname changed.
-        :param nickname: The new nickname of the user. Default is None, which will reset the nickname.
-        """
-    nickname = re.sub('<@\d+>','',ctx.message.content[5:]).strip()
+      :param context: The hybrid command ctx.
+      :param user: The user that should have its nickname changed.
+      :param nickname: The new nickname of the user. Default is None, which will reset the nickname.
+      """
+    nickname = re.sub('<@\d+>', '', ctx.message.content[5:]).strip()
     member = ctx.message.mentions[0] if ctx.message.mentions else ctx.author
     special_role = get(ctx.message.guild.roles, id=int(SPECIAL_ROLE))
     if nickname is None or nickname == '':
-      nickname = re.sub('[._ ]+','',member.name)
-    
+      nickname = re.sub('[._ ]+', '', member.name)
+
     if member.top_role.position > special_role.position:
       nickname += '' + member.top_role.name.split(' ')[-1]
 
     await member.edit(nick=nickname)
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
+
+
 async def setup(bot):
   await bot.add_cog(Moderator(bot))
--- cogs/template.py
+++ cogs/template.py
@@ -11,16 +11,16 @@ from discord.ext.commands import Context, Cog, hybrid_command
 # Here we name the cog and create a new class for the cog.
 class Template(Cog, name="Template"):
 
-	def __init__(self, bot):
-		self.bot = bot
+  def __init__(self, bot):
+    self.bot = bot
 
-	@hybrid_command(
-		test="test",
-		description="test do nothing",
-	)
-	@app_commands.describe(template="template")
-	async def test(self, ctx: Context, *, test: str) -> None:
-		pass
+  @hybrid_command(
+    test="test",
+    description="test do nothing",
+  )
+  @app_commands.describe(template="template")
+  async def test(self, ctx: Context, *, test: str) -> None:
+    pass
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
-	async def setup(bot):	
-		await bot.add_cog(Template(bot))
+  async def setup(bot):
+    await bot.add_cog(Template(bot))
