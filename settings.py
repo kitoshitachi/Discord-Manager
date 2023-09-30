8c9a8328a970122bac291dc455e39789261c0bac
kitoshitachi <19521204@gm.uit.edu.vn>
2023-09-30 15:32:41 +0700

    settings

--- settings.py
+++ settings.py
@@ -4,6 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-DISCORD_SERVER = os.getenv("DISCORD_SERVER")
 LOGS_CHANNEL = os.getenv("LOGS_CHANNEL")
 SPECIAL_ROLE = os.getenv("SPECIAL_ROLE")
