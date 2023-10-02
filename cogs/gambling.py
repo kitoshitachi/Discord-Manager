5fc6df12e2b349982947f8d3b495d1ea8433e3a0
kitoshitachi <19521204@gm.uit.edu.vn>
2023-10-03 01:07:33 +0700

    update new lvl

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -92,10 +92,10 @@ class Gambling(commands.Cog, name="gambling"):
             await self.__init_member(context, id)
         else:
             data = data[1][0]
-            embed = Embed(title="User: " + user.display_name, color=Color.red())
+            embed = Embed(title=f"{user.mention}'s Information", color=Color.red())
             embed.set_thumbnail(url=user.avatar.url)
-            embed.add_field(name="Level:", value=data['level'])
-            embed.add_field(name="Role:", value=user.top_role.name)
+            embed.add_field(name="Level", value=data['level'])
+            embed.add_field(name="Role", value=user.top_role.name)
             embed.set_footer(text="Powered by Vampire")
             await context.channel.send(embed=embed)
 
