afbbf3302d1e7d25cf271aed4f2c81bf71a572b9
kitoshitachi <19521204@gm.uit.edu.vn>
2023-10-02 12:38:22 +0700

    update template

--- cogs/template.py
+++ cogs/template.py
@@ -5,21 +5,34 @@ Description:
 
 Version: 6.1.0
 """
-from discord import app_commands
-from discord.ext.commands import Context, Cog, hybrid_command
+
+from discord.ext import commands
+from discord.ext.commands import Context
+
 
 # Here we name the cog and create a new class for the cog.
-class Template(Cog, name="Template"):
+class Template(commands.Cog, name="template"):
+    def __init__(self, bot) -> None:
+        self.bot = bot
 
-  def __init__(self, bot):
-    self.bot = bot
+    # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
 
-  @hybrid_command(
-    test="test",
-    description="test do nothing",
-  )
-  async def test(self, ctx: Context) -> None:
-    pass
+    @commands.hybrid_command(
+        name="testcommand",
+        description="This is a testing command that does nothing.",
+    )
+    async def testcommand(self, context: Context) -> None:
+        """
+        This is a testing command that does nothing.
+
+        :param context: The application command context.
+        """
+        # Do your stuff here
+
+        # Don't forget to remove "pass", I added this just because there's no content in the method.
+        pass
+
+
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
-  async def setup(bot):
+async def setup(bot) -> None:
     await bot.add_cog(Template(bot))
