bddd9b165ad849022815020ba3adebf225544b05
kitoshitachi <19521204@gm.uit.edu.vn>
2023-10-08 18:50:07 +0700

    change stat

--- classes/fantasy.py
+++ classes/fantasy.py
@@ -15,7 +15,7 @@ class FantasyWorld:
         xp = randint(self.config['MIN_EXP'], self.config['MAX_EXP'])
         cash = randint(self.config['MIN_COINS'], self.config['MAX_COINS'])
         stat = 0.001 if random() < 0.99 else 0.002
-        luck_stat = 0 if random() < (1 - 0.001) else 1
+        luck_stat = 0 if random() < (1 - 0.005) else 1
         return xp, cash, stat, luck_stat
     
     def get_stat(self, character = None):
@@ -24,7 +24,7 @@ class FantasyWorld:
         character['str'] = int(character['str'] * 5) + self.config['STR_BASE']
         character['agi'] = int(character['agi'] * 4) + self.config['AGI_BASE']
         character['def'] = int(character['def'] * 2) + self.config['DEF_BASE']
-        character['crit'] = int(character['crit'] * 1) + self.config['CRIT_BASE']
+        character['crit'] = int(character['crit'] * self.config['CRIT_BASE'])
         return character
     
     
