d05589cfd79be208db88f9d55c1124f58c4b753b
kitoshitachi <19521204@gm.uit.edu.vn>
2023-11-29 16:11:21 +0700

    update helpcommand embed

--- cores/helpcommand.py
+++ cores/helpcommand.py
@@ -5,18 +5,28 @@ from discord.ext import commands
 from discord.ext.commands import Cog, Command
 from cores.view import PaginationView
 
-from settings import PREFIX_BOT
+from settings import CONFIG
 
 class CustomHelpCommand(commands.HelpCommand):
     '''
     A custom help command for the Discord bot.
 
     '''
+    def __init__(self):
+        super().__init__(command_attrs={
+            'help': 'Shows help about the bot, a command, or a category',
+            'cooldown': commands.CooldownMapping.from_cooldown(1, 10, commands.BucketType.user)
+        })
+        self.config = CONFIG
+
     def get_command_signature(self, command: Command):
         return f"{self.context.clean_prefix} {command.name} {command.signature}"
 
     async def send_bot_help(self, mapping:Mapping[Optional[Cog], List[Command]]):
-        
+        '''
+        Sends help for the bot.
+        '''
+
         pages = []
 
         for cog, commands in mapping.items():
@@ -25,12 +35,63 @@ class CustomHelpCommand(commands.HelpCommand):
             filtered_commands = await self.filter_commands(commands, sort=True)
             command_names = [command.name for command in filtered_commands]
             if command_names:
-                cog_name = cog.qualified_name.replace(" ", "\xa0")
-                pages.append(f"**{cog_name}**\
-                             \n\n{', '.join(command_names)}\
-                             \n\n> Use `{self.context.clean_prefix} help <command>` for more info on a command.")
-        
-        await self.get_destination().send(content=pages[0], view=PaginationView(pages=pages))
+                cog_name, cog_description = cog.description.split('\n')
+
+                help_embed = discord.Embed(
+                    title=cog_name,
+                    description=cog_description,
+                    color=self.config['BLACK']
+                )
+
+                help_embed.add_field(name="List command", value=", ".join(command_names), inline=False)
+                help_embed.add_field(name='', value=f"`{self.config['NOTICE_EMOJI']} | {self.context.clean_prefix} help <command | group>`", inline=False)
+                
+                help_embed.set_footer(text='Powered by Vampire')
+
+                pages.append(help_embed)
+
+        await self.get_destination().send(embed=pages[0], view=PaginationView(pages, self.context.author.id))
+
+    async def send_cog_help(self, cog: Cog) -> None:
+        '''
+        Sends help for a specific cog.
+        '''
+        pages = []
+        filtered_commands = await self.filter_commands(cog.get_commands(), sort=True)
+        command_names = [command.name for command in filtered_commands]
+        if command_names:
+            cog_name, cog_description = cog.description.split('\n')
+            for command in filtered_commands:
+
+                help_embed = discord.Embed(
+                    title=f"{cog_name}",
+                    description=cog_description,
+                    color=self.config['BLACK']
+                )
+                
+                help_embed.add_field(name='',value='', inline=False)
+                
+
+                description_text = command.description or "No description available."
+                command_signature = self.get_command_signature(command)
+                help_embed.add_field(name=command_signature, value=description_text, inline=False)
+
+
+                for params_name, params in command.clean_params.items():
+                    help_embed.add_field(name=params_name.capitalize(), value=params.description, inline=False)
+
+
+                aliases = ", ".join(command.aliases) if command.aliases else "None"
+                help_embed.add_field(name="", value=f"**Aliases:** {aliases}", inline=False)
+
+                help_embed.add_field(name='', value=f"`{self.config['NOTICE_EMOJI']} | {self.context.clean_prefix} help <command | group>`", inline=False)
+
+                help_embed.set_footer(text='Powered by Vampire')
+                
+                pages.append(help_embed)
+
+
+        await self.get_destination().send(embed=pages[0], view=PaginationView(pages, self.context.author.id))
 
 
     async def send_command_help(self, command: commands.Command):
@@ -43,39 +104,16 @@ class CustomHelpCommand(commands.HelpCommand):
         
         help_embed = discord.Embed(title=f"{command_signature}", description=description_text)
         
+        
         for params_name, params in command.clean_params.items():
             help_embed.add_field(name=params_name.capitalize(), value=params.description, inline=False)
         
+        
         aliases = ", ".join(command.aliases) if command.aliases else "None"
         help_embed.add_field(name="Aliases", value=aliases, inline=False)
+
+
         help_embed.set_footer(text='Powered by Vampire')
+        
+        
         await self.get_destination().send(embed=help_embed)
-
-    async def send_cog_help(self, cog: Cog) -> None:
-        '''
-        Sends help for a specific cog.
-        '''
-        pages = []
-        filtered_commands = await self.filter_commands(cog.get_commands(), sort=True)
-        command_names = [command.name for command in filtered_commands]
-        if command_names:
-            cog_name = cog.qualified_name.replace(" ", "\xa0")
-            for command in filtered_commands:
-                line = f'```{cog_name}```'
-                description_text = command.description or "No description available."
-                command_signature = self.get_command_signature(command)
-                line += f'```# {command_signature}\n\n{description_text}\n'
-                
-                for params_name, params in command.clean_params.items():
-                    line += f"\n{params_name.capitalize()}: {params.description}"
-                
-                aliases = ", ".join(command.aliases) if command.aliases else "None"
-
-                line += f'\nAliases\n{aliases}```'
-                line += f'> Use `{self.context.clean_prefix} help <command>` for more info on a command.'
-                
-                pages.append(line)
-
-
-        await self.get_destination().send(content=pages[0], view=PaginationView(pages=pages))
-
