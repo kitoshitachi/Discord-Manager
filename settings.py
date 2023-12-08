e6581681195541562b3dc6dd1283a0b63863fad9
kitoshitachi <19521204@gm.uit.edu.vn>
2023-12-08 19:16:03 +0700

    prefix

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import yaml
 
 load_dotenv()
 # PREFIX_BOT = ['h','H'] #test
-PREFIX_BOT = ['v','V'] #main 
+PREFIX_BOT = 'v' #main 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
